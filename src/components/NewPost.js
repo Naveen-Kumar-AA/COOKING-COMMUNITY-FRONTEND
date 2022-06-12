97c11dc30fd2ed9afb6d18210a09c1f7f47a719a
Naveen-Kumar-AA <naveen94865@gmail.com>
2022-06-12 11:23:34 +0530

    new-post-req-sent

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -2,11 +2,19 @@ import axios from "axios"
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
-
+import { useState } from "react";
 
 
 
 const NewPost = () => {
+    const username= sessionStorage.getItem('Username')
+
+    const [title, setTitle] = useState("")
+    const [Meal, setMeals] = useState("")
+    const [cuisine, setCuisine] = useState("")
+    const [recipe, setRecipe] = useState("")
+    const [caption, setCaption] = useState("");
+
 
     const meal = [
         { label: "Breakfast" },
@@ -16,12 +24,21 @@ const NewPost = () => {
         { label: "Drinks" },
         { label: "Desserts" },
     ];
+
+
+    const sendNewPostReq = (new_post) => {
+        axios.post(`http://localhost:3001/new-post`,new_post).then((response)=>{
+            console.log(response)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
     return (
         <div style={{
-            'width' : '100%',
-            'display' : 'flex',
-            'flexDirection' : 'row',
-            'justifyContent' : 'center'
+            'width': '100%',
+            'display': 'flex',
+            'flexDirection': 'row',
+            'justifyContent': 'center'
         }}>
             <div style={{
                 'position': 'relative',
@@ -30,21 +47,31 @@ const NewPost = () => {
                 'width': '50%'
 
             }}>
-                <TextField id="outlined-basic" label="Title" variant="outlined" />
+                <TextField id="outlined-basic" label="Title" variant="outlined" onChange={(box_title) => setTitle(box_title.target.value)} />
                 <Autocomplete
                     disablePortal
                     id="combo-box-demo"
                     options={meal}
 
                     renderInput={(params) => <TextField {...params} label="Meal" />}
+                    onChange={(event, value) => {console.log(value); setMeals(value.label)}}
                 />
-                <TextField id="outlined-basic" label="Cuisine" variant="outlined" />
+                <TextField id="outlined-basic" label="Cuisine" variant="outlined" onChange ={(box_cuisine) => {setCuisine(box_cuisine.target.value)}} />
 
-                <TextField id="outlined-basic" label="Recipe" variant="outlined" />
+                <TextField id="outlined-basic" label="Recipe" variant="outlined" onChange={(box_recipe) => setRecipe(box_recipe.target.value)} />
 
-                <TextField id="outlined-basic" label="Caption" variant="outlined" />
+                <TextField id="outlined-basic" label="Caption" variant="outlined" onChange={(box_caption) => setCaption(box_caption.target.value)} />
 
-                <Button variant="contained">Create new post</Button>
+                <Button variant="contained" onClick={() => {
+                    sendNewPostReq({
+                        "username" : username,
+                        "title" : title,
+                        "meal" : Meal,
+                        "cuisine" : cuisine,
+                        "recipe" : recipe,
+                        "caption" :caption
+                    })
+                }}>Create new post</Button>
 
             </div>
         </div>
